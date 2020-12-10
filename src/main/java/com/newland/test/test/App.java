58b002938af4475e617786914fdffead300af520
cja <170863190@qq.com>
2020-12-10 23:08:54 +0800

    test commimt

--- src/main/java/com/newland/test/test/App.java
+++ src/main/java/com/newland/test/test/App.java
@@ -6,8 +6,7 @@ package com.newland.test.test;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+     public int getCount() {
+    	 return 100;
+     }
 }
